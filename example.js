fe5abbe9d4ceff0885f33b4fd3f6ebf7b901a105
Franklin Barto <kipkalisbartoo@gmail.com>
2024-07-10 17:09:12 +0300

    boilerplate with workflow

---
+++ example.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inject Button Example</title>
+</head>
+<body>
+    <!-- Your existing content -->
+
+    <!-- Include the external JavaScript file -->
+    <script src="https://franklinbart.github.io/web-disability-settings/inject.js"></script>  <!-- Replace with the actual URL of your JavaScript file -->
+
+</body>
+</html>
+
